8631f76a6b4be0eb24d0d39a0aeddb0b3e955810
nabila2009 <142683997+nabila2009@users.noreply.github.com>
2023-08-28 21:59:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-favorite_animals=["dog ,cat ,monkey ,rabbit"] 
+favorite_animals=["dog ","cat" ,"monkey" ,"rabbit"] 
 print (favorite_animals)
 print(favorite_animals[1])
+favorite_animals.remove ("monkey")
+print(favorite_animals) 
+favorite_animals.append("tiger")
+print(favorite_animals)
+for n in favorite_animals:
+  print(f"i love,{n}")
+numbers=[1,2,3,4,5]
+number_sum = 0 
+for num in numbers: 
+  number_sum += num
+
+print(number_sum)
+
+    
